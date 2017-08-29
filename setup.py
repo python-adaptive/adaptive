374e51e2323c9d445c25cc1f07d2a40558ea6868
Joseph Weston <joseph.weston08@gmail.com>
2017-08-29 11:48:58 +0200

    add setup.py and requirements file

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='adaptive',
+    description='Adaptively sample mathematical functions',
+    version='0.1a',
+
+    url='https://gitlab.kwant-project.org/qt/adaptive',
+    author='Adaptive authors',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License'
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['adaptive'],
+    install_requires=requirements,
+)
