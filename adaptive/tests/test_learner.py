ec3a91da0f5af0a57444fa8d12c54724b92ef0de
Joseph Weston <joseph.weston08@gmail.com>
2017-10-27 18:37:05 +0200

    WIP

--- adaptive/tests/test_learner.py
+++ adaptive/tests/test_learner.py
@@ -2,6 +2,18 @@
 
 from ..learner import Learner1D
 
+def test_learner1d(x):
+    # Uniform sampling in absence of data
+    # Pass the same data point in twice, state of learner does not change
+    # Pass data for a point that was not obtained from choose_points
+    # Order of calls to `add_point` in between calls to `choose_points` does not change the state
+    # Expected loss improvement is never larger than total loss
+    # Learner that never gets data outside of a certain domain should "perform similarly" to a learner *defined* over that certain domain
+    # Check that learner performance is invariant under transformations that leave the loss function invariant (rescaling domain, codomain etc.)
+    # Checking "convergence" when data points are given in a random order
+
+
+
 
 def test_neighbors():
     learner = Learner1D(lambda x: x, bounds=(-1.0, 1.0))
