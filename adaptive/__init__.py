60589534b3ed28099f97f9d4e63f63066a70abc9
Bas Nijholt <basnijholt@gmail.com>
2017-11-01 14:38:51 +0100

    fix small problems in notebook that arose in meeting

--- adaptive/__init__.py
+++ adaptive/__init__.py
@@ -4,5 +4,6 @@ from .notebook_integration import notebook_extension, live_plot
 from . import learner
 from . import runner
 
-from .learner import Learner1D, AverageLearner
+from .learner import (Learner1D, Learner2D, AverageLearner,
+                      BalancingLearner, DataSaver, IntegratorLearner)
 from .runner import Runner
