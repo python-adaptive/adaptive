2c1e73da69865c0387c84b9768357ecc176b1209
Joseph Weston <joseph.weston08@gmail.com>
2017-11-17 12:16:59 +0100

    remove unecessary 'verbose' parameters
    pytest captures stdout, and only prints it if the test fails

--- adaptive/tests/test_cquad.py
+++ adaptive/tests/test_cquad.py
@@ -16,16 +16,16 @@ def run_integrator_learner(f, a, b, tol, nr_points):
     return learner
 
 
-def same_ivals(f, a, b, tol, verbose=True):
+def same_ivals(f, a, b, tol):
         igral, err, nr_points, ivals = algorithm_4(f, a, b, tol)
 
         learner = run_integrator_learner(f, a, b, tol, nr_points)
 
-        if verbose:
-            print('igral difference', learner.igral-igral,
-                  'err difference', learner.err - err)
+        # This will only show up if the test fails, anyway
+        print('igral difference', learner.igral-igral,
+              'err difference', learner.err - err)
 
-        return learner.equal(ivals, verbose=verbose)
+        return learner.equal(ivals, verbose=True)
 
 
 def test_cquad():
@@ -33,27 +33,26 @@ def test_cquad():
                               [f7, 0, 1, 1e-6],
                               [f21, 0, 1, 1e-3],
                               [f24, 0, 3, 1e-3]]):
-        assert same_ivals(*args, verbose=True), 'Function {}'.format(i)
+        assert same_ivals(*args), 'Function {}'.format(i)
 
 
 @pytest.mark.xfail
-def test_machine_precision(verbose=True):
+def test_machine_precision():
     f, a, b, tol = [f63, 0, 1, 1e-10]
     igral, err, nr_points, ivals = algorithm_4(f, a, b, tol)
 
     learner = run_integrator_learner(f, a, b, tol, nr_points)
 
-    if verbose:
-        print('igral difference', learner.igral-igral,
-              'err difference', learner.err - err)
+    print('igral difference', learner.igral-igral,
+          'err difference', learner.err - err)
 
-    assert learner.equal(ivals, verbose=verbose)
+    assert learner.equal(ivals, verbose=True)
 
 
 def test_machine_precision2():
     f, a, b, tol = [f63, 0, 1, 1e-10]
     igral, err, nr_points, ivals = algorithm_4(f, a, b, tol)
-    
+
     learner = run_integrator_learner(f, a, b, tol, nr_points)
 
     np.testing.assert_almost_equal(igral, learner.igral)
