7f4f210263ac396e54582dd913a5f548559bc7f3
Bas Nijholt <basnijholt@gmail.com>
2017-12-16 17:10:41 +0100

    require holoviews 1.10

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ install_requires = [
     'jupyter_client>=5.2.2',  # because https://github.com/jupyter/jupyter_client/pull/314
 ]
 
-extras_require = {'recommended': ['holoviews>=1.9.1', 'ipyparallel', 'distributed']}
+extras_require = {'recommended': ['holoviews>=1.10',
+                                  'ipyparallel',
+                                  'distributed']}
 
 setup(
     name='adaptive',
