e8c4d7eb08a28e8f6245a4f098bbb981e39a4145
Joseph Weston <joseph@weston.cloud>
2018-05-24 14:59:20 +0200

    correctly calculate predicted model and error
    Add a curve to the SKOptLearner plotter that shows the
    predicted model. Update invocation of SKOptLearner in
    example notebook to use better defaults.

--- adaptive/learner/skopt_learner.py
+++ adaptive/learner/skopt_learner.py
@@ -62,28 +62,34 @@ class SKOptLearner(Optimizer, BaseLearner):
     def npoints(self):
         return len(self.Xi)
 
-    def plot(self):
+    def plot(self, nsamples=200):
         hv = ensure_holoviews()
         if self.space.n_dims > 1:
             raise ValueError('Can only plot 1D functions')
         bounds = self.space.bounds[0]
         if not self.Xi:
-            p = hv.Scatter([]) * hv.Area([])
+            p = hv.Scatter([]) * hv.Curve([]) * hv.Area([])
         else:
             scatter = hv.Scatter(([p[0] for p in self.Xi], self.yi))
             if self.models:
-                # Plot 95% confidence interval as colored area around points
                 model = self.models[-1]
-                xs = np.linspace(*bounds, 201)
-                y_pred, sigma = model.predict(np.atleast_2d(xs).transpose(),
-                                              return_std=True)
+                xs = np.linspace(*bounds, nsamples)
+                xsp = self.space.transform(xs.reshape(-1, 1).tolist())
+                y_pred, sigma = model.predict(xsp, return_std=True)
+                # Plot model prediction for function
+                curve = hv.Curve(
+                    (xs, y_pred)
+                ).opts(style=dict(line_dash='dashed'))
+                # Plot 95% confidence interval as colored area around points
                 area = hv.Area(
                     (xs, y_pred - 1.96 * sigma, y_pred + 1.96 * sigma),
                     vdims=['y', 'y2'],
                 ).opts(style=dict(alpha=0.5, line_alpha=0))
+
             else:
                 area = hv.Area([])
-            p = scatter * area
+                curve = hv.Curve([])
+            p = scatter * curve * area
 
         # Plot with 5% empty margins such that the boundary points are visible
         margin = 0.05 * (bounds[1] - bounds[0])
