bb3e0b67992e23c6e5fe8a5e52967de9d42de127
Bas Nijholt <basnijholt@gmail.com>
2018-07-10 14:43:04 -0700

    reinitialize learner in runner test

--- adaptive/tests/test_runner.py
+++ adaptive/tests/test_runner.py
@@ -7,7 +7,6 @@ from ..learner import Learner2D
 from ..runner import simple, BlockingRunner, AsyncRunner, SequentialExecutor
 
 
-
 def test_nonconforming_output():
     """Test that using a runner works with a 2D learner, even when the
     learned function outputs a 1-vector. This tests against the regression
@@ -18,11 +17,14 @@ def test_nonconforming_output():
         return [0]
 
     def goal(l):
-        return l.npoints > 1
+        return l.npoints > 10
 
     learner = Learner2D(f, [(-1, 1), (-1, 1)])
-
     simple(learner, goal)
+
+    learner = Learner2D(f, [(-1, 1), (-1, 1)])
     BlockingRunner(learner, goal, executor=SequentialExecutor())
+
+    learner = Learner2D(f, [(-1, 1), (-1, 1)])
     runner = AsyncRunner(learner, goal, executor=SequentialExecutor())
     asyncio.get_event_loop().run_until_complete(runner.task)
