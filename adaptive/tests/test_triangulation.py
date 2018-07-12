d50ef60f26cd5e198e9d98f6671276df4c13aaa7
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2018-07-12 19:47:18 +0200

    test that add_point returns changed simplices

--- adaptive/tests/test_triangulation.py
+++ adaptive/tests/test_triangulation.py
@@ -14,7 +14,7 @@ def _make_triangulation(points):
     first_simplex, points = points[:num_vertices], points[num_vertices:]
     t = Triangulation(first_simplex)
     for p in points:
-        t.add_point(p)
+        _add_point_with_check(t, p)
     return t
 
 
@@ -57,6 +57,17 @@ def _check_triangulation_is_valid(t):
     _check_simplices_are_valid(t)
     _check_faces_are_valid(t)
     _check_hull_is_valid(t)
+
+
+def _add_point_with_check(tri, point, simplex=None):
+    """Check that the difference in simplices before and after adding a point
+    is returned by tri.add_point"""
+    old_simplices = tri.simplices.copy()
+    deleted_simplices, created_simplices = tri.add_point(point, simplex=simplex)
+    new_simplices = tri.simplices.copy()
+
+    assert deleted_simplices == old_simplices - new_simplices
+    assert created_simplices == new_simplices - old_simplices
 
 
 def test_triangulation_raises_exception_for_1d_list():
@@ -102,7 +113,7 @@ def test_adding_point_outside_circumscribed_hypersphere_in_positive_orthant(dim)
     t = Triangulation(_make_standard_simplex(dim))
 
     point_outside_circumscribed_sphere = (1.1,) * dim
-    t.add_point(point_outside_circumscribed_sphere)
+    _add_point_with_check(t, point_outside_circumscribed_sphere)
 
     simplex1 = tuple(range(dim + 1))
     simplex2 = tuple(range(1, dim + 2))
@@ -128,7 +139,7 @@ def test_adding_point_outside_standard_simplex_in_negative_orthant(dim):
     t = Triangulation(_make_standard_simplex(dim))
     new_point = list(range(-dim, 0))
 
-    t.add_point(new_point)
+    _add_point_with_check(t, new_point)
 
     n_vertices = len(t.vertices)
 
@@ -163,18 +174,18 @@ def test_adding_point_inside_standard_simplex(dim, provide_simplex):
     inside_simplex = (0.1,) * dim
 
     if provide_simplex:
-        t.add_point(inside_simplex, simplex=first_simplex)
+        _add_point_with_check(t, inside_simplex, simplex=first_simplex)
     else:
-        t.add_point(inside_simplex)
+        _add_point_with_check(t, inside_simplex)
 
     added_point = dim + 1  # *index* of added point
 
     _check_triangulation_is_valid(t)
 
     other_points = list(range(dim + 1))
-    new_simplices = {(*points, added_point)
-                     for points in itertools.combinations(other_points, dim)}
-    assert new_simplices == t.simplices
+    expected_simplices = {(*points, added_point)
+                          for points in itertools.combinations(other_points, dim)}
+    assert expected_simplices == t.simplices
 
     assert np.isclose(np.sum(t.volumes()), _standard_simplex_volume(dim))
 
@@ -185,15 +196,15 @@ def test_adding_point_on_standard_simplex_face(dim):
     t = Triangulation(pts)
     on_simplex = np.average(pts[1:], axis=0)
 
-    t.add_point(on_simplex)
+    _add_point_with_check(t, on_simplex)
     added_point = dim + 1  # *index* of added point
 
     _check_triangulation_is_valid(t)
 
     other_points = list(range(1, dim+1))
-    new_simplices = {(0, *points, added_point)
-                     for points in itertools.combinations(other_points, dim-1)}
-    assert new_simplices == t.simplices
+    expected_simplices = {(0, *points, added_point)
+                          for points in itertools.combinations(other_points, dim-1)}
+    assert expected_simplices == t.simplices
 
     assert np.isclose(np.sum(t.volumes()), _standard_simplex_volume(dim))
 
@@ -204,7 +215,7 @@ def test_adding_point_on_standard_simplex_edge(dim):
     t = Triangulation(pts)
     on_edge = np.average(pts[:2], axis=0)
 
-    t.add_point(on_edge)
+    _add_point_with_check(t, on_edge)
     _check_triangulation_is_valid(t)
 
     other_points = list(range(2, dim+2))
@@ -223,7 +234,7 @@ def test_adding_point_colinear_with_first_edge(dim):
     t = Triangulation(pts)
     edge_extension = np.multiply(pts[1], 2)
 
-    t.add_point(edge_extension)
+    _add_point_with_check(t, edge_extension)
     _check_triangulation_is_valid(t)
 
     simplex1 = tuple(range(dim + 1))
@@ -238,7 +249,7 @@ def test_adding_point_coplanar_with_a_face(dim):
     t = Triangulation(pts)
     face_extension = np.sum(pts[:-1], axis=0) * 2
 
-    t.add_point(face_extension)
+    _add_point_with_check(t, face_extension)
     _check_triangulation_is_valid(t)
 
     simplex1 = tuple(range(dim + 1))
@@ -253,7 +264,7 @@ def test_adding_point_inside_circumscribed_circle(dim):
     t = Triangulation(pts)
     on_simplex = (0.6,) * dim
 
-    t.add_point(on_simplex)
+    _add_point_with_check(t, on_simplex)
     added_point = dim + 1  # *index* of added point
 
     _check_triangulation_is_valid(t)
