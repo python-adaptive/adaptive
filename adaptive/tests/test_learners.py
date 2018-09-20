896b7b4e08abe17bed5e7832f618e8dc11b5667f
Bas Nijholt <basnijholt@gmail.com>
2018-09-20 15:34:09 +0200

    move 'SKOptLearner' test to 'test_skopt_learner.py'

--- adaptive/tests/test_learners.py
+++ adaptive/tests/test_learners.py
@@ -14,12 +14,6 @@ import pytest
 from ..learner import *
 from ..runner import simple, replay_log
 
-try:
-    import skopt
-    with_scikit_optimize = True
-except ModuleNotFoundError:
-    with_scikit_optimize = False
-
 
 def generate_random_parametrization(f):
     """Return a realization of 'f' with parameters bound to random values.
@@ -127,24 +121,6 @@ def ask_randomly(learner, rounds, points):
         ls.extend(l)
 
     return xs, ls
-
-
-@pytest.mark.skipif(not with_scikit_optimize,
-                    reason='scikit-optimize is not installed')
-def test_skopt_learner_runs():
-    """The SKOptLearner provides very few guarantees about its
-       behaviour, so we only test the most basic usage
-    """
-
-    def g(x, noise_level=0.1):
-        return (np.sin(5 * x) * (1 - np.tanh(x ** 2))
-                + np.random.randn() * noise_level)
-
-    learner = SKOptLearner(g, dimensions=[(-2., 2.)])
-
-    for _ in range(11):
-        (x,), _ = learner.ask(1)
-        learner.tell(x, learner.function(x))
 
 
 @run_with(Learner1D)
