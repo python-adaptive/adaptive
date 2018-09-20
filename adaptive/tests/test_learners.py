f24018d129a0ccbd33a81eb673c4ed3bc643bde2
Bas Nijholt <basnijholt@gmail.com>
2018-09-20 13:28:17 +0000

    remove test from 'test_learners.py' that is already in 'test_learnernd.py'

--- adaptive/tests/test_learners.py
+++ adaptive/tests/test_learners.py
@@ -392,22 +392,3 @@ def test_learner_subdomain(learner_type, f, learner_kwargs):
        perform 'similarly' to learners defined on that subdomain only."""
     # XXX: not sure how to implement this. How do we measure "performance"?
     raise NotImplementedError()
-
-
-def test_faiure_case_LearnerND():
-    log = [
-        ('ask', 4),
-        ('tell', (-1, -1, -1), 1.607873907219222e-101),
-        ('tell', (-1, -1, 1), 1.607873907219222e-101),
-        ('ask', 2),
-        ('tell', (-1, 1, -1), 1.607873907219222e-101),
-        ('tell', (-1, 1, 1), 1.607873907219222e-101),
-        ('ask', 2),
-        ('tell', (1, -1, 1), 2.0),
-        ('tell', (1, -1, -1), 2.0),
-        ('ask', 2),
-        ('tell', (0.0, 0.0, 0.0), 4.288304431237686e-06),
-        ('tell', (1, 1, -1), 2.0)
-    ]
-    learner = LearnerND(lambda *x: x, bounds=[(-1, 1), (-1, 1), (-1, 1)])
-    replay_log(learner, log)
