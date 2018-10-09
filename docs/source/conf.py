c5179539893e1139b2b575ed66c9f60c6e8d9b23
Joseph Weston <joseph@weston.cloud>
2018-10-09 13:58:00 +0200

    add markdown parser to documentation building
    Also replace index.rst with a verbatim copy of the readme (for now).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,14 +47,18 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
