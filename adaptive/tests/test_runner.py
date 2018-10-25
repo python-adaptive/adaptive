0524c8d412adb5e63dcb3058400a7b9edc15ae11
Joseph Weston <joseph@weston.cloud>
2018-10-25 15:35:41 +0200

    add runner tests
    Test that runners run at all (!), and that an async function can be
    used with the AsyncExecutor.

--- adaptive/tests/test_runner.py
+++ adaptive/tests/test_runner.py
@@ -2,11 +2,39 @@
 
 import asyncio
 
-from ..learner import Learner2D
+import pytest
+
+from ..learner import Learner1D, Learner2D
 from ..runner import simple, BlockingRunner, AsyncRunner, SequentialExecutor
 
 
-def test_nonconforming_output():
+def blocking_runner(learner, goal):
+    BlockingRunner(learner, goal, executor=SequentialExecutor())
+
+
+def async_runner(learner, goal):
+    runner = AsyncRunner(learner, goal, executor=SequentialExecutor())
+    asyncio.get_event_loop().run_until_complete(runner.task)
+
+
+runners = [simple, blocking_runner, async_runner]
+
+def trivial_goal(learner):
+    return learner.npoints > 10
+
+@pytest.mark.parametrize('runner', runners)
+def test_simple(runner):
+    """Test that the runners actually run."""
+
+    def f(x):
+        return x
+    learner = Learner1D(f, (-1, 1))
+    runner(learner, lambda l: l.npoints > 10)
+    assert len(learner.data) > 10
+
+
+@pytest.mark.parametrize('runner', runners)
+def test_nonconforming_output(runner):
     """Test that using a runner works with a 2D learner, even when the
     learned function outputs a 1-vector. This tests against the regression
     flagged in https://gitlab.kwant-project.org/qt/adaptive/issues/58.
@@ -15,15 +43,14 @@ def test_nonconforming_output():
     def f(x):
         return [0]
 
-    def goal(l):
-        return l.npoints > 10
+    runner(Learner2D(f, [(-1, 1), (-1, 1)]), trivial_goal)
 
-    learner = Learner2D(f, [(-1, 1), (-1, 1)])
-    simple(learner, goal)
 
-    learner = Learner2D(f, [(-1, 1), (-1, 1)])
-    BlockingRunner(learner, goal, executor=SequentialExecutor())
+def test_aync_def_function():
 
-    learner = Learner2D(f, [(-1, 1), (-1, 1)])
-    runner = AsyncRunner(learner, goal, executor=SequentialExecutor())
+    async def f(x):
+        return x
+
+    learner = Learner1D(f, (-1, 1))
+    runner = AsyncRunner(learner, trivial_goal)
     asyncio.get_event_loop().run_until_complete(runner.task)
