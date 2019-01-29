5f9a146d57a22f4cd02f0eb6bb5afec4fa817494
Bas Nijholt <basnijholt@gmail.com>
2019-01-29 16:15:36 +0100

    add Python 3.7 to the setup.py classifiers

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=find_packages('.'),
     install_requires=install_requires,
