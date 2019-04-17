91d5f0ec4803781ce5c0bb1dd8a594b112f301bb
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2019-04-17 15:41:40 +0200

    fix spaces (after comma and indentation)

--- adaptive/tests/unit/test_learnernd.py
+++ adaptive/tests/unit/test_learnernd.py
@@ -13,22 +13,19 @@ def ring_of_fire(xy):
 
 
 def test_learnerND_inits_loss_depends_on_neighbors_correctly():
-  learner = LearnerND(ring_of_fire, bounds=[(-1, 1), (-1, 1)])
-  assert learner._loss_depends_on_neighbors == 0
+    learner = LearnerND(ring_of_fire, bounds=[(-1, 1), (-1, 1)])
+    assert learner._loss_depends_on_neighbors == 0
 
 
 def test_learnerND_curvature_inits_loss_depends_on_neighbors_correctly():
-  loss = curvature_loss_function()
-  assert loss.nth_neighbors == 1
-  learner = LearnerND(ring_of_fire, bounds=[(-1, 1), (-1, 1)], loss_per_simplex=loss)
-  assert learner._loss_depends_on_neighbors == 1
+    loss = curvature_loss_function()
+    assert loss.nth_neighbors == 1
+    learner = LearnerND(ring_of_fire, bounds=[(-1, 1), (-1, 1)], loss_per_simplex=loss)
+    assert learner._loss_depends_on_neighbors == 1
 
 
 def test_learnerND_accepts_ConvexHull_as_input():
-  triangle = ConvexHull([(0,1), (2,0), (0,0)])
-  learner = LearnerND(ring_of_fire, bounds=triangle)
-  assert learner._loss_depends_on_neighbors == 0
-  assert np.allclose(learner._bbox, [(0,2), (0,1)])
-
-
-
+    triangle = ConvexHull([(0,1), (2,0), (0,0)])
+    learner = LearnerND(ring_of_fire, bounds=triangle)
+    assert learner._loss_depends_on_neighbors == 0
+    assert np.allclose(learner._bbox, [(0,2), (0,1)])
