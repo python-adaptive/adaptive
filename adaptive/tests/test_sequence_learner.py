7cf2447e557ec6eb2e137ce2e41178f24a8d51db
Bas Nijholt <basnijholt@gmail.com>
2020-04-16 17:24:21 +0200

    define and use a FailOnce class that is callable

--- adaptive/tests/test_sequence_learner.py
+++ adaptive/tests/test_sequence_learner.py
@@ -2,23 +2,27 @@ import asyncio
 
 import numpy as np
 
-import adaptive
+from adaptive import Runner, SequenceLearner
+from adaptive.runner import SequentialExecutor
 
 
-def might_fail(dct):
-    import random
+class FailOnce:
+    def __init__(self):
+        self.failed = False
 
-    if random.random() < 0.5:
-        raise Exception()
-    return dct["x"]
+    def __call__(self, value):
+        if self.failed:
+            return value
+        self.failed = True
+        raise Exception
 
 
 def test_fail_with_sequence_of_unhashable():
     # https://github.com/python-adaptive/adaptive/issues/265
     seq = [dict(x=x) for x in np.linspace(-1, 1, 101)]  # unhashable
-    learner = adaptive.SequenceLearner(might_fail, sequence=seq)
-    runner = adaptive.Runner(
-        learner, goal=adaptive.SequenceLearner.done, retries=100
+    learner = SequenceLearner(FailOnce(), sequence=seq)
+    runner = Runner(
+        learner, goal=SequenceLearner.done, retries=100, executor=SequentialExecutor()
     )  # with 100 retries the test will fail once in 10^31
     asyncio.get_event_loop().run_until_complete(runner.task)
     assert runner.status() == "finished"
