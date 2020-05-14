34d37f7f8388cc2c0f290975aca4bd18001163a4
Bas Nijholt <basnijholt@gmail.com>
2020-05-14 16:03:08 +0200

    split up aloss and rloss

--- adaptive/learner/average_learner.py
+++ adaptive/learner/average_learner.py
@@ -118,9 +118,9 @@ class AverageLearner(BaseLearner):
         if n < self.min_npoints:
             return np.inf
         standard_error = self.std / sqrt(n)
-        return max(
-            standard_error / self.atol, standard_error / abs(self.mean) / self.rtol
-        )
+        aloss = standard_error / self.atol
+        rloss = standard_error / abs(self.mean) / self.rtol
+        return max(aloss, rloss)
 
     def _loss_improvement(self, n):
         loss = self.loss()
