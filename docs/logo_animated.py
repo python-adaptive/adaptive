bbbc07aba261f546af4fb7ff90a9210d0cb7a611
Bas Nijholt <bas@nijho.lt>
2021-09-23 17:12:48 +0200

    add rounded corners

--- docs/logo_animated.py
+++ docs/logo_animated.py
@@ -3,9 +3,30 @@ import numpy as np
 from matplotlib import animation
 from matplotlib import pyplot as plt
 from matplotlib.animation import FFMpegWriter
+from PIL import Image, ImageDraw
 from tqdm.auto import tqdm
 
 import adaptive
+
+
+def add_rounded_corners(size, rad):
+    # Make new images
+    circle = Image.new("L", (rad * 2, rad * 2), 1)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2, rad * 2), fill=0)
+    alpha = Image.new("L", size, 0)
+
+    # Crop circles
+    w, h = size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+
+    # To array
+    cut = np.array(alpha).astype(float)
+    cut[cut == 0] = np.nan
+    return cut
 
 
 def learner_till(till, learner, data):
@@ -23,12 +44,15 @@ def plot_tri(learner, ax):
     return ax.triplot(triang, c="k", lw=0.8, alpha=0.8)
 
 
-def get_new_artists(npoints, learner, data):
+def get_new_artists(npoints, learner, data, rounded_corners, ax):
     new_learner = learner_till(npoints, learner, data)
     line1, line2 = plot_tri(new_learner, ax)
     data = np.rot90(new_learner.interpolated_on_grid()[-1])
     im = ax.imshow(data, extent=(-0.5, 0.5, -0.5, 0.5), cmap="viridis")
-    return im, line1, line2
+    im2 = ax.imshow(
+        rounded_corners, extent=(-0.5, 0.5, -0.5, 0.5), cmap="gray_r", zorder=10
+    )
+    return im, line1, line2, im2
 
 
 def create_and_run_learner():
@@ -53,11 +77,17 @@ if __name__ == "__main__":
     fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
     ax.set_xticks([])
     ax.set_yticks([])
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
 
     nseconds = 15
     npoints = (len(data) * np.linspace(0, 1, 24 * nseconds) ** 2).astype(int)
-
-    artists = [get_new_artists(n, learner, data) for n in tqdm(npoints)]
+    rounded_corners = add_rounded_corners(size=(1000, 1000), rad=300)
+    artists = [
+        get_new_artists(n, learner, data, rounded_corners, ax) for n in tqdm(npoints)
+    ]
 
     ani = animation.ArtistAnimation(fig, artists, blit=True)
-    ani.save("logo.mp4", writer=FFMpegWriter(fps=24))
+    ani.save("movie.mp4", writer=FFMpegWriter(fps=24))
