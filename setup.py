8de6b1db9fa72027aadc9091f05ae5bf3698106a
Bas Nijholt <bas@nijho.lt>
2022-11-28 09:59:16 -0800

    Add support for Python 3.11 and test on it

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=find_packages("."),
     install_requires=install_requires,
