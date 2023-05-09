2b941528e57dbf753d5a73e398fb3d5b7cbb0a5e
Bas Nijholt <bas@nijho.lt>
2023-05-09 16:22:55 -0700

    Allow storing the full sequence in SequenceLearner.to_dataframe (#425)
    * Allow storing the full sequence in SequenceLearner.to_dataframe
    
    * Test dataframes
    
    * skip if minimal deps

--- adaptive/tests/test_sequence_learner.py
+++ adaptive/tests/test_sequence_learner.py
@@ -1,7 +1,17 @@
 import asyncio
 
+import pytest
+
 from adaptive import Runner, SequenceLearner
-from adaptive.runner import SequentialExecutor
+from adaptive.learner.learner1D import with_pandas
+from adaptive.runner import SequentialExecutor, simple
+
+offset = 0.0123
+
+
+def peak(x, offset=offset, wait=True):
+    a = 0.01
+    return {"x": x + a**2 / (a**2 + (x - offset) ** 2)}
 
 
 class FailOnce:
@@ -22,3 +32,27 @@ def test_fail_with_sequence_of_unhashable():
     runner = Runner(learner, retries=1, executor=SequentialExecutor())
     asyncio.get_event_loop().run_until_complete(runner.task)
     assert runner.status() == "finished"
+
+
+@pytest.mark.skipif(not with_pandas, reason="pandas is not installed")
+def test_save_load_dataframe():
+    learner = SequenceLearner(peak, sequence=range(10, 30, 1))
+    simple(learner, npoints_goal=10)
+    df = learner.to_dataframe()
+    assert len(df) == 10
+    assert df["x"].iloc[0] == 10
+    df_full = learner.to_dataframe(full_sequence=True)
+    assert len(df_full) == 20
+    assert df_full["x"].iloc[0] == 10
+    assert df_full["x"].iloc[-1] == 29
+
+    learner2 = learner.new()
+    assert learner2.data == {}
+    learner2.load_dataframe(df)
+    assert len(learner2.data) == 10
+    assert learner.to_dataframe().equals(df)
+
+    learner3 = learner.new()
+    learner3.load_dataframe(df_full, full_sequence=True)
+    assert len(learner3.data) == 10
+    assert learner3.to_dataframe(full_sequence=True).equals(df_full)
