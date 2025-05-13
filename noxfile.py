5167fe0c5cfafd85aa5cc3d6a7c9018b978aacae
Bas Nijholt <bas@nijho.lt>
2025-05-13 11:16:58 +0200

    Use `uv` as Nox backend and several related improvements (#476)

--- noxfile.py
+++ noxfile.py
@@ -1,31 +1,45 @@
 """Nox configuration file."""
 
+import os
+
 import nox
 
+nox.options.default_venv_backend = "uv"
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
-@nox.parametrize("all_deps", [True, False])
-def pytest(session: nox.Session, all_deps: bool) -> None:
-    """Run pytest with optional dependencies."""
-    session.install(".[testing,other]" if all_deps else ".[testing]")
+python = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+num_cpus = os.cpu_count() or 1
+xdist = ("-n", "auto") if num_cpus > 2 else ()
+
+
+@nox.session(python=python)
+def pytest_min_deps(session: nox.Session) -> None:
+    """Run pytest with no optional dependencies."""
+    session.install(".[test]")
     session.run("coverage", "erase")
-    session.run("pytest")
+    session.run("pytest", *xdist)
 
 
-@nox.session(python="3.11")
+@nox.session(python=python)
+def pytest_all_deps(session: nox.Session) -> None:
+    """Run pytest with "other" optional dependencies."""
+    session.install(".[test,other]")
+    session.run("coverage", "erase")
+    session.run("pytest", *xdist)
+
+
+@nox.session(python="3.13")
 def pytest_typeguard(session: nox.Session) -> None:
     """Run pytest with typeguard."""
-    session.install(".[testing,other]")
+    session.install(".[test,other]")
     session.run("coverage", "erase")
-    session.run("pytest", "--typeguard-packages=adaptive")
+    session.run("pytest", "--typeguard-packages=adaptive", *xdist)
 
 
-@nox.session(python="3.11")
+@nox.session(python="3.13")
 def coverage(session: nox.Session) -> None:
     """Generate coverage report."""
-    session.install("coverage")
-    session.install(".[testing,other]")
-    session.run("pytest")
+    session.install(".[test,other]")
+    session.run("pytest", *xdist)
 
     session.run("coverage", "report")
     session.run("coverage", "xml")
